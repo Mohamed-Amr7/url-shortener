5ade705cfa9b32afff5d6519ae78a2082c21c37e
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-03-01 01:06:23 +0200

    Fixed the .env path

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: '../.env' }); // Load environment variables
+dotenv.config({ path: './.env' }); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Use port from .env or default to 3000
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
