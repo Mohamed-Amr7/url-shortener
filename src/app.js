4681d06c37cd36eb31f6010605026bea7526ab7a
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-02-29 23:11:11 +0200

    Fixed dotenv.config path

--- src/app.js
+++ src/app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: '.env' }); // Load environment variables
+dotenv.config({ path: '../.env' }); // Load environment variables
 
 const app = express();
-const port = process.env.PORT || 3000; // Use port from .env or default to 3000
-
+const PORT = process.env.PORT || 3000; // Use port from .env or default to 3000
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -15,6 +14,4 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Error connecting to MongoDB:', err));
 
-// Your application routes and middleware go here
-
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
