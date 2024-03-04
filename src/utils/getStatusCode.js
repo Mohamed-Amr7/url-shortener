b306900fde34c7f8053120f9973dbacc58b1acdc
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-03-04 22:48:03 +0200

    Add more error status codes for MongoDB errors

--- src/utils/getStatusCode.js
+++ src/utils/getStatusCode.js
@@ -1,8 +1,12 @@
-getStatusCode = (err)=> {
+getStatusCode = (err) => {
     if (err.name === 'ValidationError') {
         return 400; // Bad Request for validation errors
     } else if (err.name === 'MongoError') {
-        return 500; // Internal Server Error for database errors
+        if (err.code === 11000) { // Duplicate key error
+            return 409; // Conflict
+        } else if (err.code === 26 || err.code === 11001) { // Document not found
+            return 404; // Not Found
+        }
     } else {
         return 500; // Default status code for other errors
     }
