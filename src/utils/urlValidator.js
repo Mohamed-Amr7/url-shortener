b3bf71fd3b6ac10f1fbee76b0343734b9abc8e84
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-03-04 23:47:57 +0200

    Remove the TypeError thrown in urlValidator helper function

--- src/utils/urlValidator.js
+++ src/utils/urlValidator.js
@@ -1,12 +1,10 @@
 /**
  * Checks if the provided input is a valid URL.
-
+ *
  * @param {string} inputUrl - The URL string to be validated.
-
+ *
  * @returns {boolean} True if the input is a valid URL, false otherwise.
-
- * @throws {TypeError} - If the input is not a string.
-
+ *
  * @example
  * ```javascript
  * const isValid = isValidUrl('https://www.example.com');
@@ -15,17 +13,9 @@
  * const isInvalid = isValidUrl('not a url');
  * console.log(isInvalid); // Output: false
  *
- * try {
- *   isValidUrl(123); // This will throw a TypeError
- * } catch (error) {
- *   console.error(error.message); // Output: isValidUrl: url must be a string
- * }
  * ```
  */
 function isValidUrl(inputUrl) {
-    if (typeof inputUrl !== 'string') {
-        throw new TypeError('isValidUrl: url must be a string');
-    }
     try {
         new URL(inputUrl);
         return true
