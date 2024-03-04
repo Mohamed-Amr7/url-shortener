08762ba55d1603c08f9b83ad8cad11158a0b76c5
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-03-04 22:51:19 +0200

    Create errorMiddleware to add the error status code and error message to the response

---
+++ src/utils/errorMiddleware.js
@@ -0,0 +1,11 @@
+const getStatusCode = require('getStatusCode');
+const getErrorMessage = require('getErrorMessage');
+
+errorMiddleware = (err, req, res) => {
+    const statusCode = getStatusCode(err);
+    const message = getErrorMessage(err);
+
+    res.status(statusCode).json({ message });
+};
+
+module.exports = errorMiddleware
