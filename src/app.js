974fdab7386aaf7a0292bd0cbfd34259ef8e91d2
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-03-06 23:27:26 +0200

    Add errorMiddleware and routes to app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const errorMiddleware = require('./utils/errorMiddleware')
+const routes = require("./routes/index");
 dotenv.config({ path: './.env' }); // Load environment variables
 
-const app = express();
-const PORT = process.env.PORT || 3000; // Use port from .env or default to 3000
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -15,4 +14,13 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Error connecting to MongoDB:', err));
 
+const app = express();
+
+app.use(express.json())
+app.use(routes)
+app.use(errorMiddleware)
+
+const PORT = process.env.PORT || 3000; // Use port from .env or default to 3000
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
