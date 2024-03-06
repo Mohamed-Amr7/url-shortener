757d5fd8a63abb73089197323b0cf38092bb5347
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-03-07 01:00:33 +0200

    Add redirect router to index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express")
 const router = express.Router()
 const allRouter = require('./all')
 const shortRouter = require('./short')
+const redirectRouter = require('./redirect')
 
 router.use(allRouter)
 router.use(shortRouter)
+router.use(redirectRouter)
 
 module.exports = router
