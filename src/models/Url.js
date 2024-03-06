2ac3bd0f29a9b10afffb6a2bd2eb4a2b801c935b
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-03-06 21:41:39 +0200

    Add documentation to the Url schema

--- src/models/Url.js
+++ src/models/Url.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 
-// Schema for storing URL shortening information
+/**
+ * Mongoose schema for a URL document.
+ *
+ * @typedef {Object} Url
+ * @property {string} origUrl - The original, long URL. (Required)
+ * @property {string} shortUrl - The shortened version of the original URL. (Required)
+ * @property {string} hash - A unique hash used to identify the shortened URL. (Required)
+ * @property {number} clicks - The number of times the shortened URL has been clicked. (Default: 0)
+ * @property {Date} date - The date and time the URL document was created. (Default: current date/time)
+ */
 const UrlSchema = new mongoose.Schema({
     origUrl: {
         type: String,
