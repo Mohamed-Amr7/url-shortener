f3bc7e75c87f645134b648c0f437c5ddf7e6f83b
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-03-06 23:51:18 +0200

    Add getUrlByHash

--- src/handlers/urls.js
+++ src/handlers/urls.js
@@ -21,17 +21,32 @@ const getUrls = async () => {
 /**
  * Asynchronously retrieves a specific URL document from the database based on its original URL.
  *
- * @param {string} url - The original URL to search for.
+ * @param {string} origUrl - The original URL to search for.
  * @throws {Error} - Re-throws any errors encountered during the database operation.
  * @returns {Promise<Url>} - A promise that resolves to the matching `Url` document if found, or `null` otherwise.
  */
-const getUrl = async (url) => {
+const getUrlByOriginal = async (origUrl) => {
     try {
-        return await Url.findOne({origUrl: url})
+        return await Url.findOne({origUrl});
     } catch (err) {
         throw err; // Re-throw for middleware to handle
     }
-}
+};
+
+/**
+ * Asynchronously retrieves a specific URL document from the database based on its hash.
+ *
+ * @param {string} hash - The hash portion of the URL to search for.
+ * @throws {Error} - Re-throws any errors encountered during the database operation.
+ * @returns {Promise<Url>} - A promise that resolves to the matching `Url` document if found, or `null` otherwise.
+ */
+const getUrlByHash = async (hash) => {
+    try {
+        return await Url.findOne({ hash });
+    } catch (err) {
+        throw err; // Re-throw for middleware to handle
+    }
+};
 
 /**
  * Asynchronously creates a new URL document in the database.
@@ -66,5 +81,6 @@ const createUrl = async ({origUrl, base, hash, date = Date(), clicks = 0}) => {
 module.exports = {
     createUrl,
     getUrls,
-    getUrl,
+    getUrlByOriginal,
+    getUrlByHash,
 }
