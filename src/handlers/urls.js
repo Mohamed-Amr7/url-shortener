78814f2b9bf188baf45aca0a026b89ecaa85fe1a
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-03-09 17:40:25 +0200

    Add getUrlById function to url handlers

--- src/handlers/urls.js
+++ src/handlers/urls.js
@@ -49,6 +49,22 @@ const getUrlByHash = async (hash) => {
 };
 
 /**
+* Asynchronously retrieves a specific URL document from the database based on its MongoDB ID.
+*
+* @param {ObjectId} id - The MongoDB ID of the URL to search for.
+* @throws {Error} - Re-throws any errors encountered during the database operation.
+* @returns {Promise<Url>} - A promise that resolves to the matching `Url` document if found, or `null` otherwise.
+*/
+const getUrlById = async (id) => {
+    try {
+        // Explicitly create an ObjectId instance for accurate querying
+        return await Url.findOne({ _id: id });
+    } catch (err) {
+        throw err; // Re-throw for middleware to handle
+    }
+};
+
+/**
  * Asynchronously creates a new URL document in the database.
  *
  * @param {object} data - An object containing properties for the new URL document:
@@ -83,4 +99,5 @@ module.exports = {
     getUrls,
     getUrlByOriginal,
     getUrlByHash,
+    getUrlById,
 }
