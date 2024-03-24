791e8733a3e449f5c3c3901ff2b3fe73960bb384
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-03-24 02:48:44 +0200

    Implement page for displaying all URLs

---
+++ client/src/pages/AllUrlsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UrlList from "../components/UrlList";
+
+const AllUrlsPage = () => {
+    return (
+        <div>
+            <UrlList />
+        </div>
+    );
+}
+
+export default AllUrlsPage;
